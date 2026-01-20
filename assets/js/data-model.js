e29e7e728324b0611ef9551194dd9cc542c856b5
BobywanCompensator <guillaume.fauno@gmail.com>
2026-01-20 23:58:58 +0100

    Update data-model.js

--- assets/js/data-model.js
+++ assets/js/data-model.js
@@ -1,1 +1,82 @@
+/**
+ * data-model.js
+ * --------------------------------------------------
+ * Accès structuré et lisible aux données CSV
+ * Dépend de : CSVLoader
+ * --------------------------------------------------
+ */
+
+const DataModel = (() => {
+
+  /* =========================
+     DONNÉES BRUTES
+  ========================= */
+
+  function factions() {
+    return CSVLoader.get('factions');
+  }
+
+  function warlords() {
+    return CSVLoader.get('warlords');
+  }
+
+  function sectors() {
+    return CSVLoader.get('sectors');
+  }
+
+  function battles() {
+    return CSVLoader.get('battles');
+  }
+
+  function journal() {
+    return CSVLoader.get('journal');
+  }
+
+  /* =========================
+     HELPERS SIMPLES
+  ========================= */
+
+  function factionById(id) {
+    return factions().find(f => f.id === id) || null;
+  }
+
+  function warlordById(id) {
+    return warlords().find(w => w.id === id) || null;
+  }
+
+  function sectorById(id) {
+    return sectors().find(s => s.id === id) || null;
+  }
+
+  function battlesForSector(sectorId) {
+    return battles().filter(b => b.sector === sectorId);
+  }
+
+  function battlesForWarlord(warlordId) {
+    return battles().filter(
+      b => b.attacker === warlordId || b.defender === warlordId
+    );
+  }
+
+  /* =========================
+     API PUBLIQUE
+  ========================= */
+
+  return {
+    // accès directs
+    factions,
+    warlords,
+    sectors,
+    battles,
+    journal,
+
+    // helpers
+    factionById,
+    warlordById,
+    sectorById,
+    battlesForSector,
+    battlesForWarlord
+  };
+
+})();
 
